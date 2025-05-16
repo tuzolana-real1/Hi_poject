593436a8376867187e4242494b7f70fc682b766e
tuzolana-real1 <fidelreal38@gmail.com>
2025-05-16 17:12:58 +0100

    tabuada dos dois

---
+++ Hi_World/src/main/java/com/mycompany/hi_world/Hi_World.java
@@ -0,0 +1,10 @@
+package com.mycompany.hi_world;
+public class Hi_World {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, farei a tabuada!");
+        for (int i = 0; i < 12; i++) {
+            System.out.println(i*2);
+        }
+    }
+}
